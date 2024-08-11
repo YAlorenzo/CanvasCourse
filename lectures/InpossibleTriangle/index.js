0129c9918d3c7f953952b7ed3944f11d51709e79
YAlorenzo <zhenia.klimenchuk@gmail.com>
2024-08-11 19:11:06 +0200

    addRightCLick

--- lectures/InpossibleTriangle/index.js
+++ lectures/InpossibleTriangle/index.js
@@ -1,8 +1,18 @@
 const context = myCanvas.getContext("2d");
 
+myCanvas.addEventListener("pointerdown", (e) => {
+  if (e.button !== 2) {
+    context.globalCompositeOperation = 'source-over';
+  }
+  else {
+    context.globalCompositeOperation = "destination-over";
+  }
+  drawCube(e.offsetX, e.offsetY, 100, e.button == 2);
+})
+
 drawCube(myCanvas.width / 2, myCanvas.height / 2, 100);
 
-function drawCube(centerX, centerY, size) {
+function drawCube(centerX, centerY, size, topPertInBack = false) {
   context.beginPath();
   const radius = size / 2;
   for (let i = 0; i <= 6; i++) {
@@ -16,6 +26,9 @@ function drawCube(centerX, centerY, size) {
     }
 
     if (i == 2 || i == 4 || i == 6) {
+      if (i === 6 && topPertInBack) {
+        context.globalCompositeOperation = "destination-over";
+      }
       context.stroke();
       context.lineTo(centerX, centerY);
       context.fillStyle = ["#888",  "#444", "#AAA"][i / 2 - 1];
